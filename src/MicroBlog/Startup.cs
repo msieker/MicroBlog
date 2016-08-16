c99d1a8019ada5ee0a2261adc4a6d2a57620c861
Matt Sieker <msieker@NERDERY.AD.BRAVO>
2016-08-16 18:32:26 -0500

    Routes

--- src/MicroBlog/Startup.cs
+++ src/MicroBlog/Startup.cs
@@ -74,7 +74,12 @@ namespace MicroBlog
 
             app.UseStaticFiles();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
