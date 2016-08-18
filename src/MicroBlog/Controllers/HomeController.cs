dc4e789127606d6f27db4bb08c374c079d448b2b
Matt Sieker <msieker@NERDERY.AD.BRAVO>
2016-08-18 11:44:27 -0500

    Styling! Paging! Stuff!

--- src/MicroBlog/Controllers/HomeController.cs
+++ src/MicroBlog/Controllers/HomeController.cs
@@ -3,22 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace MicroBlog.Controllers
 {    
     public class HomeController : Controller
     {
         private readonly IPostRepository _postRepository;
-        
-        public HomeController(IPostRepository postRepository)
+        private readonly Settings _settings;
+        public HomeController(IPostRepository postRepository, IOptions<Settings> settings)
         {
             _postRepository = postRepository;
+            _settings = settings.Value;
         }
 
- 
-        public IActionResult Index()
+        [Route("")]
+        [Route("page/{pageNum}")]
+        [Route("{category}")]
+        [Route("{category}/{pageNum}")]
+        public IActionResult Index(string category, int? pageNum)
         {
-            return View(_postRepository.AllPosts);
+            var posts = _postRepository.AllPosts;
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                posts = posts.Where(p => p.Category.Equals(category, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (pageNum.HasValue)
+            {
+                posts = posts.Skip(pageNum.Value * _settings.PageSize);
+            }
+
+            posts = posts.Take(_settings.PageSize);
+            return View(posts);
         }
     }
 }
